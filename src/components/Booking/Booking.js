339674aac8467203d0805ba2ab41654b0ca2aa6e
unknown <montasirazad@gmail.com>
2022-04-28 06:13:42 +0600

    json data problem fixed

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -1,51 +1,66 @@
-import React, { useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import '../../App.css';
+import Button from '@mui/material/Button';
 // MU imports /// 
 import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import React, { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import '../../App.css';
+
 
 
 const Booking = () => {
 
+    const [selectedDate, setSelectedDate] = useState({
+        checkInDate: new Date(),
+        checkOutDate: new Date()
+    })
+   
 
-    const [locale, setLocale] = React.useState('ru');
-    const [value, setValue] = React.useState(new Date());
+    const { title } = useParams();
 
-    const selectLocale = (newLocale) => {
-        setLocale(newLocale);
-    };
+    const handleCheckInDate = date => {
+        const newDate = { ...selectedDate }
+        newDate.checkInDate = date;
+        setSelectedDate(newDate);
+    }
 
-    const { title } = useParams()
+    const handleCheckOutDate = date => {
+        const newDate = { ...selectedDate }
+        newDate.checkOutDate = date;
+        setSelectedDate(newDate);
+    }
+    const handleBooking = () => {
+        console.log(selectedDate);
+    }
     return (
         <div className='App'>
+            
             <h3>Thanks for choosing {title}</h3>
+            <Link to='/'><p>Want another room ...?</p></Link> <br /><br />
 
             <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <DatePicker
                     label="CHECK -IN DATE"
-                    value={value}
-                    onChange={(newValue) => {
-                        setValue(newValue);
-                    }}
+                    value={selectedDate.checkInDate}
+                    onChange={handleCheckInDate}
                     renderInput={(params) => <TextField {...params} />}
+                    inputFormat='dd/MM/yyyy'
                 />
             </LocalizationProvider>
 
             <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <DatePicker
                     label="CHECK -OUT DATE"
-                    value={value}
-                    onChange={(newValue) => {
-                        setValue(newValue);
-                    }}
+                    value={selectedDate.checkOutDate}
+                    onChange={handleCheckOutDate}
                     renderInput={(params) => <TextField {...params} />}
+                    inputFormat='dd/MM/yyyy'
                 />
-            </LocalizationProvider>
+            </LocalizationProvider> <br /> <br />
 
-
+            <Button onClick={handleBooking} variant="outlined" color="primary" size="large">BOOK NOW </Button>
         </div>
     );
 };
